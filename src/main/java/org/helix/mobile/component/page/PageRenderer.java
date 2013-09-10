8d4a704ad85faebcacae16bd4d08fa18d3c6c1ed
Seth Hallem <seth@mobilehelix.com>
2013-09-10 08:01:58 -0400

    Bug fix

--- src/main/java/org/helix/mobile/component/page/PageRenderer.java
+++ src/main/java/org/helix/mobile/component/page/PageRenderer.java
@@ -102,8 +102,8 @@ public class PageRenderer extends CoreRenderer {
         // Then override with pf-mobile content.
         renderResource(context, "helix-mobile-full.css", "javax.faces.resource.Stylesheet", HelixLibraryName, null);
         renderResource(context, "css/helix.overrides.css", "javax.faces.resource.Stylesheet", HelixLibraryName, null);
-        renderResource(context, "helix-mobile-full.js", "javax.faces.resource.Script", HelixLibraryName, null);
         renderResource(context, "cordova-full.js", "javax.faces.resource.Script", HelixLibraryName, null);
+        renderResource(context, "helix-mobile-full.js", "javax.faces.resource.Script", HelixLibraryName, null);
         
         writer.startElement("script", null);
         writer.writeAttribute("type", "text/javascript", null);
