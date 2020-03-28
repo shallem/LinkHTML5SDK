156620c329b9122fe11dccd03f05ab769ef060ea
Seth Hallem <seth@mobilehelix.com>
2020-03-28 09:02:33 +0000

    Added basic android support.

---
+++ src/main/resources/META-INF/resources/helix/cordova/cordova-android.js
@@ -0,0 +1,29 @@
+function cordova_android_init() {
+    window.cordova = {
+        exec: function(success, failure, objName, methodName, args) {
+            objName = "Native" + objName;
+            if (objName in window) {
+                var obj = window[objName];
+                var finalArgs = [];
+                for (var i = 0; i < args.length; ++i) {
+                    var nxt = args[i];
+                    if ($.isArray(nxt)) {
+                        finalArgs.push(JSON.stringify(nxt));
+                    } else if (nxt !== null && typeof nxt === 'object') {
+                        finalArgs.push(JSON.stringify(nxt));
+                    } else {
+                        finalArgs.push(nxt);
+                    }
+                }
+                
+                var resStr = obj[methodName].apply(obj, finalArgs);
+                var res = $.parseJSON(resStr);
+                if (res.code === 0) {
+                    success(res.val ? $.parseJSON(res.val) : null);
+                } else {
+                    failure(res.msg);
+                }
+            }
+        }
+    };
+}
